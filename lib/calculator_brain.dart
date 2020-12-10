3d0106890a848ae48788f88f5847ff74b15e273c
Nikhil Nishad <nikhilnishad279@gmail.com>
2020-12-11 00:59:03 +0530

    Updated manifest to latest flutter requirments + BMI calculation UI+Functionality added
    Signed-off-by: Nikhil Nishad <nikhilnishad279@gmail.com>

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,24 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  final height;
+  final weight;
+  double _bmi;
+
+  CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+}
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,65 @@
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  final String bmiResults;
+
+  final String resultText;
+
+  const ResultsPage({@required this.bmiResults, @required this.resultText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text('Your Result', style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResults,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your BMI is low1 Eat More',
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
